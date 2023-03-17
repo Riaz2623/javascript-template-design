481bdc091e344f0c148451f0d5cc2e4cce58154c
Ayeshaenterprises <riazul002@gmail.com>
2023-03-18 02:38:43 +0600

    JavaScrept HTML CSS

---
+++ index.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="src/style.css">
+
+</head>
+<body>
+<div class="navbar">
+    <a href="index.php">
+        <h2>Clothing Store</h2>
+    </a>
+<a href="cart.php">
+    <div class="cart">
+        <i class="bi bi-cart"></i>
+        <div class="cardAmount" id="cardAmount">10</div>
+    </div>
+</a>
+
+</div>
+
+<div class="shop" id="shop">
+
+
+
+
+</div>
+</body>
+<script src="src/Data.js"></script>
+<script src="src/main.js"></script>
+</html>
